192f78a343e9db902f2991aec92fa0c23b3bdf7b
Manuel - Escoltes i guies <manuel-cau@pop-os.localdomain>
2022-01-26 17:56:33 +0100

    Primera versio

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/msardi-meg/verify-auth
+
+go 1.17
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
---
+++ main.go
@@ -0,0 +1,35 @@
+package verifyAuth
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var mySigningKey = []byte(os.Getenv("JWT_SIGNKEY"))
+
+func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Authorization"] != nil {
+
+			token, err := jwt.Parse(r.Header["Authorization"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("there was an error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				http.Error(w, "Not authorized", http.StatusUnauthorized)
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			http.Error(w, "Not authorized", http.StatusUnauthorized)
+		}
+	})
+}
